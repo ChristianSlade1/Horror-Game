196d0109fe60dcbdc815886c8821726845103b96
ChristianSlade1 <107496840+ChristianSlade1@users.noreply.github.com>
2022-06-26 18:56:30 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-$(".select").click(function() {
+$(".selectA").click(function() {
     $(".all1").fadeOut();
-    $(".all2").delay(500).fadeIn( 400 );
+    $(".all2").delay(500).fadeIn(400);
 });
+//#2
+$(".selectB").click(function() {
+    $(".all2").fadeOut();
+    $(".eye").delay(5000).show(1).delay(49).hide(1);
+    $(".all3").delay(7000).fadeIn(400);
+});
+$(".select1B").click(function() {
+    $(".all2").fadeOut();
+    $(".eye").delay(5000).show(1).delay(49).hide(1);
+});
+
 
 
 
