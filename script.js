cda20c24905229cdf944fa71f091b3412711d8b7
ChristianSlade1 <107496840+ChristianSlade1@users.noreply.github.com>
2022-06-26 23:43:32 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -15,14 +15,35 @@ $(".select1B").click(function() {
 });
 $(".selectC").click(function() {
     $(".all4").fadeOut();
-    $(".garden").fadeOut();
-    $(".deathtext").delay(3000).fadeIn(500);
+    $(".garden1").fadeOut();
+    $(".deathtextA").delay(3000).fadeIn(500);
     $(".deathtext1").delay(4000).fadeIn(500);
     var audio=document.getElementById("goofy");
     audio.src="https://audio.jukehost.co.uk/8NaJP45gxeVANvdzQS2DzVBaeu8iDIsc";  
     audio.play();
 });
-
+$(".select1C").click(function() {
+    $(".all4").fadeOut();
+    $(".garden").fadeOut();
+    $(".reaper1").delay(500).fadeIn(400);
+    $(".all5").delay(500).fadeIn(400);
+});
+$(".select1D").click(function() {
+    $(".all5").fadeOut();
+    $(".reaper").fadeOut();
+    $(".deathtextB").delay(3000).fadeIn(500);
+    $(".deathtext2").delay(4000).fadeIn(500);
+    var audio=document.getElementById("stab");
+    audio.src="https://audio.jukehost.co.uk/DVELi7RQRvbnY0ZwBkMLIOUZSv1ea3Dl";  
+    audio.play();
+});
+$(".selectD").click(function() {
+    $(".all5").fadeOut();
+    $(".reaper").fadeOut();
+    $(".wintext").delay(1000).fadeIn(500);
+    $(".wintext1").delay(2000).fadeIn(500);    
+});
+    
 
 
 
